f1d5bb6092acf07351c9be5ed027fe71640066db
sultan <sultanhasanov987@gmail.com>
2022-06-08 10:55:13 +0300

    Создан объект soldier

---
+++ game-soldier.js
@@ -0,0 +1,18 @@
+const soldier = {
+    name: 'Raybek',
+    health: 10,
+    weapon: {
+        nameWeapon: 'Ак-47',
+        cartridges: 30
+    },
+    supplies: 3,
+    fire: function (){
+
+    },
+    recharge: function (){
+
+    },
+    hurt: function () {
+        
+    }
+}
